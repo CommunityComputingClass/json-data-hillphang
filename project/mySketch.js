e21fa7296a2edec52d4d3995db44313faff90fb4
hillphang <26hillary.phang@lwhs.org>
2025-04-03 22:31:49 +0000

    notes

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,10 +1,15 @@
+let names = ["apple", "apricot","avocado","banana","bell pepper", "bilberry","blackberry", "blackcurrant",]
+let y = 50; 
 
 function setup() {
   createCanvas(400, 400);
-  background(200);
- 
-}
-
-function draw(){
   
+  background(179, 215, 255)
+  fill("white")
+  textSize(16)
+  
+  for(let n = 0; n<names.length; n++){
+    text(names[n], 150, y);
+    y += 50; 
+  }    
 }
